aa4c26218eac6078d5764b6165991e8103c05c88
Баранов Максим <m.baranov>
2023-12-25 18:25:59 +0200

    improvement problem 03

--- Chapter01/problem_03/problem_03_test.cpp
+++ Chapter01/problem_03/problem_03_test.cpp
@@ -2,8 +2,15 @@
 #include <numeric>
 #include "solution.h" 
 
-TEST(ProblemTest, TEST1) { 
+TEST(ProblemTest, two_numbers) { 
     ASSERT_EQ(solution(10, 20), std::lcm(10, 20));
     ASSERT_EQ(solution(10, 6), std::lcm(10, 6));
     ASSERT_EQ(solution(144, 14), std::lcm(144, 14));
 } 
+
+TEST(ProblemTest, vec) {
+    ASSERT_EQ(solution({6,10,20}), 60);
+    ASSERT_EQ(solution({3,4,5}),60);
+    ASSERT_EQ(solution({56,12,48}), 336);
+    ASSERT_EQ(solution({3,4,5,6,7,8,9}), 2520);
+}
--- Chapter01/problem_03/solution.h
+++ Chapter01/problem_03/solution.h
@@ -1,7 +1,12 @@
 // Write a program that will, given two or more positive integers, calculate and print the least
 // common multiple of them all.
 #include <numeric>
+#include <vector>
 
 int solution(int a, int b) {
     return a * b / std::gcd(a, b);
+}
+
+int solution(std::vector<int> v) {
+    return std::accumulate(v.begin(), v.end(), 1, [](int a, int b) {return std::lcm(a, b); });
 }
